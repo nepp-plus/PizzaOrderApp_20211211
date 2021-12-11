0281bb33bed3b88607a703d7c35588ba6f14acce
nepp-plus <nepp.plus@gmail.com>
2021-12-11 14:53:05 +0900

    실제 가게 데이터 상세화면 표시

--- app/src/main/java/com/neppplus/pizzaorderapp_20211211/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/ViewStoreDetailActivity.kt
@@ -2,8 +2,14 @@ package com.neppplus.pizzaorderapp_20211211
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.neppplus.pizzaorderapp_20211211.datas.StoreData
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : AppCompatActivity() {
+
+    lateinit var mStoreData : StoreData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
@@ -16,6 +22,12 @@ class ViewStoreDetailActivity : AppCompatActivity() {
     }
 
     fun setValues() {
+        mStoreData =  intent.getSerializableExtra("store") as StoreData
+
+        txtStoreName.text =  mStoreData.name
+        txtPhoneNum.text =  mStoreData.phone
+
+        Glide.with(this).load(mStoreData.logoURL).into(imgLogo)
 
     }
 
