df50a312c8804711227bb1f52e65902658c299bf
nepp-plus <nepp.plus@gmail.com>
2021-12-11 14:36:56 +0900

    가게 상세보기 화면 추가

---
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/ViewStoreDetailActivity.kt
@@ -0,0 +1,22 @@
+package com.neppplus.pizzaorderapp_20211211
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStoreDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_store_detail)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+
+    }
+
+}
