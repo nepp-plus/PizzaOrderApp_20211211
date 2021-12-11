f44864bb94e29959d86155cf18ff14bf2e5b567d
nepp-plus <nepp.plus@gmail.com>
2021-12-11 15:15:58 +0900

    프래그먼트 - 결과 가지러 이동,  닉네임변경액티비티 - 완료 누르면 결과 설정 / 복귀

--- app/src/main/java/com/neppplus/pizzaorderapp_20211211/EditNicknameActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/EditNicknameActivity.kt
@@ -1,7 +1,9 @@
 package com.neppplus.pizzaorderapp_20211211
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,18 @@ class EditNicknameActivity : AppCompatActivity() {
     }
 
     fun setupEvents() {
+
+        btnOk.setOnClickListener {
+
+            val inputNickname =  edtNickname.text.toString()
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick", inputNickname)
+
+            setResult( RESULT_OK, resultIntent )
+            finish()
+
+        }
 
     }
 
